bd1b5cfb026b83a0d6c6d15d9e0118fe49e8cd09
WHOISCOOKS <alvianputraleksana@gmail.com>
2024-05-15 13:04:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/api10/TeamResponse.java
@@ -0,0 +1,11 @@
+package com.example.api10;
+
+import java.util.List;
+
+public class TeamResponse {
+    private List<Team> teams;
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+}
